83e71d95813c230f6148af4bdbd50fcab2b68287
Rafał Cieślak <ravicious@gmail.com>
2020-10-11 19:34:53 +0200

    Add config.ru

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # markdown-twitter-thread
 Export a Twitter thread to Markdown format
+
+## Development
+
+To start the app, install the gems with `bundle` and then:
+
+```
+bundle exec rerun rackup
+```
--- app.rb
+++ app.rb
@@ -1,41 +1,44 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/flash'
 
-enable :sessions
+class App < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
 
-get '/' do
-  erb :index
-end
-
-post '/tweets' do
-  id_or_url = params.fetch(:id)
-  tweet_id = parse_tweet_id(id_or_url)
-
-  if tweet_id
-    redirect to("/tweets/#{tweet_id}")
-  else
-    flash[:error] = "Couldn't find a Tweet ID in the given URL"
-    redirect '/'
+  get '/' do
+    erb :index
   end
-end
 
-get '/tweets/:id' do
-  tweet_id = parse_string_to_integer(params[:id])
+  post '/tweets' do
+    id_or_url = params.fetch(:id)
+    tweet_id = parse_tweet_id(id_or_url)
 
-  erb :'tweets/show', locals: {
-    tweet_id: tweet_id
-  }
-end
+    if tweet_id
+      redirect to("/tweets/#{tweet_id}")
+    else
+      flash[:error] = "Couldn't find a Tweet ID in the given URL"
+      redirect '/'
+    end
+  end
 
-TWEET_ID_REGEX = /(\d+)\z/
+  get '/tweets/:id' do
+    tweet_id = parse_string_to_integer(params[:id])
 
-def parse_tweet_id(string)
-  match = TWEET_ID_REGEX.match(string)
-  match && match[0]&.to_i
-end
+    erb :'tweets/show', locals: {
+      tweet_id: tweet_id
+    }
+  end
 
-def parse_string_to_integer(string)
-  int = string.to_i
+  TWEET_ID_REGEX = /(\d+)\z/
 
-  string == int.to_s && int
+  def parse_tweet_id(string)
+    match = TWEET_ID_REGEX.match(string)
+    match && match[0]&.to_i
+  end
+
+  def parse_string_to_integer(string)
+    int = string.to_i
+
+    string == int.to_s && int
+  end
 end
