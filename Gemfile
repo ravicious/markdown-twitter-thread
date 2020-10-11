b589fb8d700228f853ae7c62e90817cc1d2570e6
Rafał Cieślak <ravicious@gmail.com>
2020-10-11 14:02:07 +0200

    Add parsing Tweet ID

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra"
+gem "sinatra-flash"
+
 gem "faraday"
 gem "faraday_middleware"
 
--- app.rb
+++ app.rb
@@ -1,5 +1,41 @@
 require 'sinatra'
+require 'sinatra/flash'
+
+enable :sessions
 
 get '/' do
   erb :index
+end
+
+post '/tweets' do
+  id_or_url = params.fetch(:id)
+  tweet_id = parse_tweet_id(id_or_url)
+
+  if tweet_id
+    redirect to("/tweets/#{tweet_id}")
+  else
+    flash[:error] = "Couldn't find a Tweet ID in the given URL"
+    redirect '/'
+  end
+end
+
+get '/tweets/:id' do
+  tweet_id = parse_string_to_integer(params[:id])
+
+  erb :'tweets/show', locals: {
+    tweet_id: tweet_id
+  }
+end
+
+TWEET_ID_REGEX = /(\d+)\z/
+
+def parse_tweet_id(string)
+  match = TWEET_ID_REGEX.match(string)
+  match && match[0]&.to_i
+end
+
+def parse_string_to_integer(string)
+  int = string.to_i
+
+  string == int.to_s && int
 end
